a64e87c9d8f3c608a00bf243aa8ed261a5f0095f
mechiru <mechiru@users.noreply.github.com>
2022-08-18 18:39:18 +0000

    google-api-proto: regenerate code

--- google-api-proto/src/google/iam/v2beta/mod.rs
+++ google-api-proto/src/google/iam/v2beta/mod.rs
@@ -5,7 +5,7 @@ pub struct DenyRule {
     /// Google Cloud resources. This field can contain the following values:
     ///
     /// * `principalSet://goog/public:all`: A special identifier that represents
-    ///   any user who is on the internet, even if they do not have a Google
+    ///   any principal that is on the internet, even if they do not have a Google
     ///   Account or are not logged in.
     ///
     /// * `principal://goog/subject/{email_id}`: A specific Google Account.
